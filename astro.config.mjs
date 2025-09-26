aa54a89724bf475a228040d0c4f50974a1b63b3c
Jonatan <j.jhonis95@gmail.com>
2025-09-26 13:12:02 -0300

    adding website map

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,13 @@ import alpinejs from '@astrojs/alpinejs';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs({ entrypoint: '/src/entrypoint' })],
+
+  site: 'https://refrigeracaonoroeste.com.br',
+  integrations: [alpinejs({ entrypoint: '/src/entrypoint' }), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
